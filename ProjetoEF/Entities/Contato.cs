e0e42ab02875b56aa82975054a38c2b102a01b51
DaniloMP10 <zdanilo2014@hotmail.com>
2023-01-24 21:28:16 -0300

    Projeto CRUD finalizado

---
+++ ProjetoEF/Entities/Contato.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEF.Entities
+{
+    public class Contato
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
